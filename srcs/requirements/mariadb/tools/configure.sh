7ecb19723d47d5a514d599909b5ae485c93cf3da
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-08-09 13:39:31 +0200

    Completing docker-compose

---
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start MySQL service
+service mysql start
+
+# Execute SQL commands
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+#exec mysqld_safe
