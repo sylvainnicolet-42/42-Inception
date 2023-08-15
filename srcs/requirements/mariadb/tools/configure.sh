d23d62758d498e5e8decc55855be2011b18f1550
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-08-15 12:31:01 +0200

    Adding mariadb config

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-# Start MySQL service
-service mysql start
+# Start SQL service
+service mysql start;
 
-# Execute SQL commands
+# Create a database (if the database does not exist)
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# Create an user with a password (if the user does not exist)
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Give all privileges to the user
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Modify SQL database
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# Reload the database
 mysql -e "FLUSH PRIVILEGES;"
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-#exec mysqld_safe
+
+# Shutdown
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+# Start SQL service
+exec mysqld_safe
