232c2a20463365b11fe74d1ff9f839e908bc5927
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-08-16 18:07:50 +0200

    WIP

--- Makefile
+++ Makefile
@@ -5,8 +5,6 @@ DOCKER_COMPOSE = ./srcs/docker-compose.yml
 
 all: up
 
-rebuild:	delete_volumes build
-
 up:
 		docker-compose -f ${DOCKER_COMPOSE} -p inception up -d
 
@@ -16,12 +14,14 @@ build:
 down:
 		docker-compose -f ${DOCKER_COMPOSE} -p inception down
 
-create_volumes:
+create_data:
 	mkdir -p $(VOLUMES_PATH)/wordpress
 	mkdir -p $(VOLUMES_PATH)/mysql
 
-delete_volumes:
+delete_data:
 	rm -rf $(VOLUMES_PATH)
+
+clean_data: delete_data create_data
 
 purge:
 	docker image prune -f -a
@@ -30,17 +30,19 @@ purge_volumes:
 	docker volume rm inception_wordpress
 	docker volume rm inception_mariadb
 
+fclean: purge purge_volumes clean_data
+
 help:
 	@echo "Usage: make [target]"
 	@echo ""
 	@echo "Targets:"
 	@echo "up: Runs the containers"
 	@echo "build: Builds and run the containers"
-	@echo "rebuild: Delete and rebuilds the containers"
 	@echo "down: Stops and removes the containers"
-	@echo "create_volumes: Creates the volumes"
-	@echo "delete_volumes: Deletes the volumes"
+	@echo "clean_data: Deletes and create data folders"
+	@echo "create_data: Creates data folders"
+	@echo "delete_data: Deletes data folders"
 	@echo "purge: Deletes all images"
 	@echo "purge_volumes: Deletes all volumes"
+	@echo "fclean: Deletes all images and data folders"
 	@echo "help: Shows help"
-	@echo ""
--- srcs/requirements/wordpress/tools/conf_wordpress.sh
+++ srcs/requirements/wordpress/tools/conf_wordpress.sh
@@ -6,15 +6,33 @@ sleep 10
 # Check if wordpress is already installed
 if [ ! -e /var/www/wordpress/wp-config.php ]
 then
-    wp config create
-      --allow-root \
-      --dbname=${SQL_DATABASE} \
-      --dbuser=${SQL_USER} \
-      --dbpass=${SQL_PASSWORD} \
-    	--dbhost=${SQL_HOST} \
-    	--path='/var/www/wordpress'
+  # First page
+  wp config create --allow-root \
+    --dbname=${SQL_DATABASE} \
+    --dbuser=${SQL_USER} \
+    --dbpass=${SQL_PASSWORD} \
+    --dbhost=${SQL_HOST} \
+    --dbcharset="utf8" \
+    --dbcollate="utf8_general_ci" \
+    --path='/var/www/wordpress'
 
-#sleep 2
-#wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-#wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+  sleep 2
+
+  # Second page
+  wp core install --allow-root \
+    --url=${WP_SITE_URL} \
+    --title=${WP_SITE_TITLE} \
+    --admin_user=${WP_ADMIN_USER} \
+    --admin_password=${WP_ADMIN_PASSWORD} \
+    --admin_email=${WP_ADMIN_EMAIL} \
+    --skip-email \
+    --path='/var/www/wordpress'
+
+  sleep 2
+
+  # Create user
+  wp user create --allow-root \
+    --role=author ${WP_USER} ${WP_USER_EMAIL} \
+    --user_pass=${WP_USER_PASSWORD} \
+    --path='/var/www/wordpress'
 fi
